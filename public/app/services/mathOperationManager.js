290d8f17be01f19f9324828232ac74c3037bc39e
ppoliani <ppoliani@gmail.com>
2015-02-21 13:36:21 +0000

    mathOperarionManager unit tests added

--- public/app/services/mathOperationManager.js
+++ public/app/services/mathOperationManager.js
@@ -9,12 +9,19 @@
         // region Inner Methods
 
         /**
-         * Performs the math operation based on the passed string
+         * Performs the math operation based on the passed array
          * which follows a fixed pattern e.g. 5 plus 5
          * @param operation
          */
         function getResult(operation){
-            throw 'Not Implemented';
+            switch(operation[1]){
+                case 'plus':
+                    return operation[0] + operation [2];
+                case 'minus':
+                    return operation[0] - operation [2];
+                case 'times':
+                    return operation[0] * operation [2];
+            }
         }
 
         // endregion
--- public/app/tests/mathOperationManager.specs.js
+++ public/app/tests/mathOperationManager.specs.js
@@ -10,13 +10,35 @@ describe('The mathOperationManager service', function(){
     describe('The getResult method', function(){
         it('Should return the correct plus operation result', function(){
             // Arrange
-            var operation = '5 plus 5';
+            var operation = [5, 'plus', 5];
 
             // Act
             var result = mathOperationManager.getResult(operation);
 
             //Assert
             expect(result).to.equal(10);
+        });
+
+        it('Should return the correct minus operation result', function(){
+            // Arrange
+            var operation = [5, 'minus', 5];
+
+            // Act
+            var result = mathOperationManager.getResult(operation);
+
+            //Assert
+            expect(result).to.equal(0);
+        });
+
+        it('Should return the correct times operation result', function(){
+            // Arrange
+            var operation = [5, 'times', 5];
+
+            // Act
+            var result = mathOperationManager.getResult(operation);
+
+            //Assert
+            expect(result).to.equal(25);
         });
     });
 });
