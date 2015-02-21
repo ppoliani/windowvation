9e84799b9e4d12aa2c1f7cff79632d7d318841d8
ppoliani <ppoliani@gmail.com>
2015-02-21 13:46:24 +0000

    responseParser api added

--- public/app/services/responseParser.js
+++ public/app/services/responseParser.js
@@ -8,12 +8,31 @@
 
         // region Inner Methods
 
+        /**
+         * Parses the response and return the HTTP method and the
+         * url of the next request
+         * @param response
+         */
+        function parseResponse(response){
+            throw 'Not Implemented';
+        }
+
+        /**
+         * Parses the question response and return the HTTP method, the
+         * url of the next request and the operation to be answered
+         * @param response
+         */
+        function parseQuestionResponse(response){
+            throw 'Not Implemented';
+        }
+
         // endregion
 
         // region Public API
 
         return {
-
+            parseResponse: parseResponse,
+            parseQuestionResponse: parseQuestionResponse
         };
 
         // endregion
